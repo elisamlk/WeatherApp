638714975910536b9b37bccbdfce7dd4aa93a4b1
Elisabeth Malek <elisabeth.malek@yahoo.fr>
2023-03-20 21:16:23 +0100

    Ajout de la route add city

--- routes/index.js
+++ routes/index.js
@@ -17,4 +17,14 @@ router.get("/weather", function (req, res, next) {
   res.render("weather", { cityList });
 });
 
+router.post("/add-city", function (req, res, next) {
+  cityList.push({
+    name: req.body.newcity,
+    desc: "Couvert",
+    img: "/images/pic2.jpg",
+    temp: 24,
+  });
+  res.render("weather", { cityList });
+});
+
 module.exports = router;
